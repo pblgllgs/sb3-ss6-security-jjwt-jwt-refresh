da0f0a8c30ab30f0b33ab73b42942f68ba7d011b
pblgllgs <pbl.gllgs@gmail.com>
2024-03-27 15:53:54 -0300

    change version of jjwt 0.11.5 -> 0.12.5

--- src/main/java/com/pblgllgs/security/services/impl/JwtServiceImpl.java
+++ src/main/java/com/pblgllgs/security/services/impl/JwtServiceImpl.java
@@ -3,14 +3,10 @@ package com.pblgllgs.security.services.impl;
 import com.pblgllgs.security.services.JwtService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 import java.util.function.Function;
@@ -24,22 +20,16 @@ import java.util.function.Function;
 @Service
 public class JwtServiceImpl implements JwtService {
 
-    @Value("${secret-key}")
-    private String secretKey;
-
-    private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
-    }
+    SecretKey key = Jwts.SIG.HS256.key().build();
 
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
-                .setSubject(userDetails.getUsername())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .subject(userDetails.getUsername())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .signWith(key)
                 .compact();
     }
-
 
     private <T> T extractClaims(String token, Function<Claims, T> claimsFunction) {
         final Claims claims = extractAllClaims(token);
@@ -48,7 +38,7 @@ public class JwtServiceImpl implements JwtService {
 
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
     }
 
     public String extractUsername(String token) {
@@ -63,11 +53,12 @@ public class JwtServiceImpl implements JwtService {
 
     public String refreshToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 604800000))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 604800000))
+                .subject(userDetails.getUsername())
+                .signWith(key)
                 .compact();
     }
 
